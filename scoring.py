4b091aefe202d4d75473bea13cd760d06fba019d
Philip Vegdahl <pvegdahl@gmail.com>
2022-02-19 16:00:59 -0500

    Refactor to separate meld counting and scoring

--- scoring.py
+++ scoring.py
@@ -1,23 +1,49 @@
-from typing import List
+from typing import List, NamedTuple
 
 from cards import Card, Suit, Rank
 
 
+class Meld(NamedTuple):
+    nines_of_trump: int = 0
+    marriages: int = 0
+
+
 def score_meld(hand: List[Card], trump: Suit) -> int:
-    meld = count_nines_of_trump(hand, trump)
-    for suit in Suit:
-        meld += count_marriages_in_suit(hand, suit) * 2
-    return meld
+    meld = MeldCounter(hand=hand, trump=trump).count()
+    score = meld.nines_of_trump + meld.marriages * 2
+    return score
 
 
-def count_nines_of_trump(hand: List[Card], trump: Suit) -> int:
-    return len([card for card in hand if card == Card(Rank.NINE, trump)])
+class MeldCounter:
+    def __init__(self, hand: List[Card], trump: Suit):
+        self.hand = hand
+        self.trump = trump
 
+    def count(self) -> Meld:
+        return Meld(
+            nines_of_trump=self._nines_of_trump(), marriages=self._all_marriages()
+        )
 
-def count_marriages_in_suit(hand: List[Card], suit: Suit) -> int:
-    queen_count = len([card for card in hand if card.rank == Rank.QUEEN and card.suit == suit])
-    king_count = len([card for card in hand if card.rank == Rank.KING and card.suit == suit])
-    if queen_count and king_count:
-        return int((queen_count + king_count) / 2)
-    return 0
+    def _nines_of_trump(self):
+        return len([card for card in self.hand if card == Card(Rank.NINE, self.trump)])
 
+    def _all_marriages(self):
+        result = 0
+        for suit in Suit:
+            result += self._marriages_in_suit(suit)
+        return result
+
+    def _marriages_in_suit(self, suit: Suit):
+        queen_count = len(
+            [
+                card
+                for card in self.hand
+                if card.rank == Rank.QUEEN and card.suit == suit
+            ]
+        )
+        king_count = len(
+            [card for card in self.hand if card.rank == Rank.KING and card.suit == suit]
+        )
+        if queen_count and king_count:
+            return int((queen_count + king_count) / 2)
+        return 0
