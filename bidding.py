5168240eac534f99bfbd9a97d187c175b95bd0d0
Philip Vegdahl <pvegdahl@gmail.com>
2022-02-21 14:12:54 -0500

    Add validation that bids only increase

--- bidding.py
+++ bidding.py
@@ -1,8 +1,14 @@
 from typing import NamedTuple
 
 
+class InvalidBid(Exception):
+    pass
+
 class BiddingState(NamedTuple):
-    current_bid: int = 0
+    current_bid: int
 
     def new_bid(self, bid) -> "BiddingState":
+        if bid <= self.current_bid:
+            raise InvalidBid(f"New bid of {bid} did not exceed the current bid of {self.current_bid}")
+
         return self._replace(current_bid=bid)
--- tests/test_bidding.py
+++ tests/test_bidding.py
@@ -1,7 +1,25 @@
-from bidding import BiddingState
+import pytest
+
+from bidding import BiddingState, InvalidBid
 
 
 def test_bid_updates_current_bid():
-    bid = BiddingState()
+    bid = BiddingState(current_bid=25)
     bid = bid.new_bid(26)
     assert bid.current_bid == 26
+
+
+def test_reject_lower_bids():
+    bid = BiddingState(current_bid=25)
+    with pytest.raises(InvalidBid) as e:
+        bid.new_bid(24)
+    assert e.match("New bid of 24 did not exceed the current bid of 25")
+
+
+def test_reject_equal_bids():
+    bid = BiddingState(current_bid=25)
+    with pytest.raises(InvalidBid) as e:
+        bid.new_bid(25)
+    assert e.match("New bid of 25 did not exceed the current bid of 25")
+
+
