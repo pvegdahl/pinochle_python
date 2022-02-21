3755b732f45c6620a6ac18ffe2da9f31344d0a5f
Philip Vegdahl <pvegdahl@gmail.com>
2022-02-21 14:25:29 -0500

    Rotate through bidders

--- bidding.py
+++ bidding.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
 
 
 class InvalidBid(Exception):
@@ -7,11 +7,19 @@ class InvalidBid(Exception):
 
 class BiddingState(NamedTuple):
     current_bid: int
+    players: Tuple[str, str, str, str]
+    current_bidder_index: int = 0
 
-    def new_bid(self, bid) -> "BiddingState":
+    def new_bid(self, bid: int) -> "BiddingState":
         if bid <= self.current_bid:
             raise InvalidBid(
                 f"New bid of {bid} did not exceed the current bid of {self.current_bid}"
             )
 
-        return self._replace(current_bid=bid)
+        return self._replace(current_bid=bid, current_bidder_index=self._get_next_bidder_index())
+
+    def _get_next_bidder_index(self):
+        return (self.current_bidder_index + 1) % 4
+
+    def current_bidder(self) -> str:
+        return self.players[self.current_bidder_index]
--- tests/test_bidding.py
+++ tests/test_bidding.py
@@ -4,24 +4,36 @@ from bidding import BiddingState, InvalidBid
 
 
 @pytest.fixture(scope="function")
-def bid():
-    return BiddingState(current_bid=25)
+def bidding_state() -> BiddingState:
+    return BiddingState(current_bid=25, players=("a", "b", "c", "d"))
 
 
-def test_bid_updates_current_bid(bid):
-    bid = bid.new_bid(26)
-    assert bid.current_bid == 26
+def test_bid_updates_current_bid(bidding_state: BiddingState) -> None:
+    bidding_state = bidding_state.new_bid(26)
+    assert bidding_state.current_bid == 26
 
 
-def test_reject_lower_bids(bid):
+def test_reject_lower_bids(bidding_state: BiddingState) -> None:
     with pytest.raises(InvalidBid) as e:
-        bid.new_bid(24)
+        bidding_state.new_bid(24)
     assert e.match("New bid of 24 did not exceed the current bid of 25")
 
 
-def test_reject_equal_bids(bid):
+def test_reject_equal_bids(bidding_state: BiddingState) -> None:
     with pytest.raises(InvalidBid) as e:
-        bid.new_bid(25)
+        bidding_state.new_bid(25)
     assert e.match("New bid of 25 did not exceed the current bid of 25")
 
 
+def test_update_current_bidder(bidding_state: BiddingState) -> None:
+    assert bidding_state.current_bidder() == "a"
+    bidding_state = bidding_state.new_bid(26)
+    assert bidding_state.current_bidder() == "b"
+    bidding_state = bidding_state.new_bid(27)
+    assert bidding_state.current_bidder() == "c"
+    bidding_state = bidding_state.new_bid(28)
+    assert bidding_state.current_bidder() == "d"
+    bidding_state = bidding_state.new_bid(29)
+    assert bidding_state.current_bidder() == "a"
+
+# def test_reject_bids_by_wrong_player()
