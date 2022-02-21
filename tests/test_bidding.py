d63f4adbe42e8005d5007fd59e94e97698742bea
Philip Vegdahl <pvegdahl@gmail.com>
2022-02-21 14:16:19 -0500

    Add a bid pytest fixture
    As the BiddingState gets more complex, we can update initial state in
    just one place.

--- tests/test_bidding.py
+++ tests/test_bidding.py
@@ -3,21 +3,25 @@ import pytest
 from bidding import BiddingState, InvalidBid
 
 
-def test_bid_updates_current_bid():
-    bid = BiddingState(current_bid=25)
+@pytest.fixture(scope="function")
+def bid():
+    return BiddingState(current_bid=25)
+
+
+def test_bid_updates_current_bid(bid):
     bid = bid.new_bid(26)
     assert bid.current_bid == 26
 
 
-def test_reject_lower_bids():
-    bid = BiddingState(current_bid=25)
+def test_reject_lower_bids(bid):
     with pytest.raises(InvalidBid) as e:
         bid.new_bid(24)
     assert e.match("New bid of 24 did not exceed the current bid of 25")
 
 
-def test_reject_equal_bids():
-    bid = BiddingState(current_bid=25)
+def test_reject_equal_bids(bid):
     with pytest.raises(InvalidBid) as e:
         bid.new_bid(25)
     assert e.match("New bid of 25 did not exceed the current bid of 25")
+
+
