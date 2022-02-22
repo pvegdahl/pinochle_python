c78b481e73b1a8b20d53410bddf1fe74a78d2ab3
Philip Vegdahl <pvegdahl@gmail.com>
2022-02-22 16:59:25 -0500

    [CLEANUP] Delete duplicated code in pinochole_game.py

--- pinochle_game.py
+++ pinochle_game.py
@@ -37,27 +37,6 @@ class PinochleGame(NamedTuple):
     def select_trump(self, player: str, trump: Suit) -> "PinochleGame":
         return self._replace(trump=trump, state=GameState.PASSING_TO_BID_WINNER)
 
-    def pass_cards(
-        self, source: str, destination: str, cards: Tuple[Card, Card, Card, Card]
-    ) -> "PinochleGame":
-        self._validate_legality_of_pass(source=source, destination=destination, cards=cards)
-
-        new_hand_0 = self.hands[0] + cards
-        new_hand_2 = self._remove_cards_from_hand(
-            initial_hand=self.hands[2], cards_to_remove=cards
-        )
-        new_hands = (new_hand_0, self.hands[1], new_hand_2, self.hands[3])
-        return self._replace(hands=new_hands)
-
-    def _validate_legality_of_pass(self, source: str, destination: str, cards: Tuple[Card, Card, Card, Card]):
-        if len(cards) != 4:
-            raise IllegalPass(f"Passes must be exactly 4 cards, not {len(cards)}")
-        if self.state != GameState.PASSING_TO_BID_WINNER:
-            raise IllegalPass(f"You cannot pass cards in the {self.state} phase")
-
-        if source != "c" or destination != "a":
-            raise IllegalPass(f"The only legal pass is from {self._get_bid_winner_partner()} to {self._get_bid_winner()}")
-
     def _get_bid_winner(self) -> str:
         return self.bidding.get_winner()
 
@@ -65,19 +44,3 @@ class PinochleGame(NamedTuple):
         bid_winner_index = self.players.index(self._get_bid_winner())
         partner_index = (bid_winner_index + 2) % 4
         return self.players[partner_index]
-
-    @classmethod
-    def _remove_cards_from_hand(
-        cls, initial_hand: Tuple[Card, ...], cards_to_remove: Tuple[Card, ...]
-    ) -> Tuple[Card, ...]:
-        if not cards_to_remove:
-            return initial_hand
-        try:
-            index_to_remove = initial_hand.index(cards_to_remove[0])
-        except ValueError as e:
-            raise IllegalPass(f"{cards_to_remove[0]} is not in hand to pass")
-
-        new_hand = initial_hand[:index_to_remove] + initial_hand[index_to_remove + 1:]
-        return cls._remove_cards_from_hand(
-            initial_hand=new_hand, cards_to_remove=cards_to_remove[1:]
-        )
