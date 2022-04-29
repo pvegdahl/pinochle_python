f47f1c8b834bfa63ae965404bb56ac445fe6becb
Philip Vegdahl <pvegdahl@gmail.com>
2022-04-28 21:50:33 -0400

    Dedicated trick functions are working

--- tests/test_trick.py
+++ tests/test_trick.py
@@ -1,20 +1,33 @@
 import pytest
 
 from cards import Card, Rank, Suit
-from trick import get_trick_winner
+from trick import get_trick_winner, get_trick_winner_index
 
 
-@pytest.mark.parametrize("cards, expected", [
+@pytest.mark.parametrize("cards, expected_card", [
     ((Card(Rank.KING, Suit.DIAMONDS), Card(Rank.ACE, Suit.DIAMONDS)), Card(Rank.ACE, Suit.DIAMONDS)),
     ((Card(Rank.ACE, Suit.SPADES), Card(Rank.QUEEN, Suit.SPADES)), Card(Rank.ACE, Suit.SPADES)),
     ((Card(Rank.KING, Suit.DIAMONDS), Card(Rank.ACE, Suit.HEARTS)), Card(Rank.KING, Suit.DIAMONDS)),
 ])
-def test_highest_card_in_led_suit_wins(cards, expected):
-    assert get_trick_winner(cards=cards) == expected
+def test_highest_card_in_led_suit_wins(cards, expected_card):
+    assert get_trick_winner(cards=cards, trump=Suit.CLUBS) == expected_card
 
 
-# TODO
-# - Trump beats non trump
-# - Higher trump wins
-# - Off suit not trump always loses
-# - First of duplicate cards wins
+@pytest.mark.parametrize("cards, expected_card", [
+    ((Card(Rank.ACE, Suit.SPADES), Card(Rank.NINE, Suit.CLUBS)), Card(Rank.NINE, Suit.CLUBS)),
+    ((Card(Rank.ACE, Suit.SPADES), Card(Rank.TEN, Suit.CLUBS), Card(Rank.ACE, Suit.CLUBS)), Card(Rank.ACE, Suit.CLUBS)),
+])
+def test_trump_beats_non_trump(cards, expected_card):
+    assert get_trick_winner(cards=cards, trump=Suit.CLUBS) == expected_card
+
+
+@pytest.mark.parametrize("cards, expected_index", [
+    ((Card(Rank.KING, Suit.DIAMONDS), Card(Rank.ACE, Suit.DIAMONDS)), 1),
+    ((Card(Rank.ACE, Suit.SPADES), Card(Rank.QUEEN, Suit.SPADES)), 0),
+    ((Card(Rank.KING, Suit.DIAMONDS), Card(Rank.ACE, Suit.HEARTS)), 0),
+    ((Card(Rank.ACE, Suit.SPADES), Card(Rank.NINE, Suit.CLUBS)), 1),
+    ((Card(Rank.ACE, Suit.SPADES), Card(Rank.TEN, Suit.CLUBS), Card(Rank.ACE, Suit.CLUBS)), 2),
+    ((Card(Rank.KING, Suit.DIAMONDS), Card(Rank.ACE, Suit.DIAMONDS), Card(Rank.ACE, Suit.DIAMONDS)), 1),
+])
+def test_get_trick_winner_index(cards, expected_index):
+    assert get_trick_winner_index(cards=cards, trump=Suit.CLUBS) == expected_index
--- trick.py
+++ trick.py
@@ -1,10 +1,16 @@
 from typing import Tuple
 
-from cards import Card
+from cards import Card, Suit
 
 
-def get_trick_winner(cards: Tuple[Card, ...]) -> Card:
-    led_suit = cards[0].suit
-    matching_cards = [card for card in cards if card.suit == led_suit]
+def get_trick_winner(cards: Tuple[Card, ...], trump: Suit) -> Card:
+    trump_cards = [card for card in cards if card.suit == trump]
+    if trump_cards:
+        return max(trump_cards)
+
+    matching_cards = [card for card in cards if card.suit == cards[0].suit]
     return max(matching_cards)
 
+
+def get_trick_winner_index(cards: Tuple[Card, ...], trump: Suit) -> int:
+    return cards.index(get_trick_winner(cards, trump))
